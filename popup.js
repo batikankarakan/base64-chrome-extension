e650a982a3a942e2c8d680f27e1bfa5c6b7edbe8
Batikan Karakan <batikankarakan@hotmail.com>
2018-02-24 21:23:08 +0100

    Added event listener to encode and decode buttons.

--- popup.js
+++ popup.js
@@ -1,20 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
-    }, false);
-  }, false);
+document.addEventListener('DOMContentLoaded', function () {
+  // add event listener to encode button.
+  var encodeButton = document.getElementById('encode');
+  encodeButton.addEventListener('click', function () {
+    var inputValue = document.getElementById("input-text").value;
+    var encodedString = btoa(inputValue);
+    console.log(encodedString);
+  });
+
+  // add event listener to decode button.
+  var decodeButton = document.getElementById('decode');
+  decodeButton.addEventListener('click', function () {
+    var inputValue = document.getElementById("input-text").value;
+    var decodedString = atob(inputValue);
+    console.log(decodedString);
+  });
+});
